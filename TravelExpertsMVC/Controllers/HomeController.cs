0e4c7931c1e7d6e897f490a4482581469aa3429d
Taofeek <oduola.taofeekkola@gmail.com>
2024-12-09 12:56:54 -0700

    initial styling

--- TravelExpertsMVC/Controllers/HomeController.cs
+++ TravelExpertsMVC/Controllers/HomeController.cs
@@ -20,17 +20,31 @@ namespace TravelExpertsMVC.Controllers
 
         public IActionResult Index()
         {
+            // Set the current page's controller and action in ViewData
+            ViewData["ActiveController"] = "Home";
+            ViewData["ActiveAction"] = "Index";
             ViewBag.Packages = PackageManager.GetPackages(_context);
             return View();
         }
 
         public IActionResult Privacy()
         {
+            // Set the current page's controller and action in ViewData
+            ViewData["ActiveController"] = "Home";
+            ViewData["ActiveAction"] = "Privacy";
+            return View();
+        }
+        public IActionResult Register()
+        {
+            // Set the current page's controller and action in ViewData
+            ViewData["ActiveController"] = "Home";
+            ViewData["ActiveAction"] = "Register";
             return View();
         }
 
         public IActionResult Bookings()
         {
+            
 
             return View();
         }
